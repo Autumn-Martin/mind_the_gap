feff7d731f142b3d45b2f35f29cafe4fe1f641d7
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-29 13:53:41 -0700

    Test reservation validations & relationships

--- spec/models/campsite_spec.rb
+++ spec/models/campsite_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Campsite, type: :model do
   describe 'validations' do
     it {should validate_presence_of :name}
-  end 
+    it {should have_many :reservations}
+  end
 end
