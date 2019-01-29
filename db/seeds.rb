9667b568ca89b3817c0bda12d3a82fa2cfb878eb
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-29 14:53:31 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.start
+    clear_prior_data
+    seed_data
+    puts "Successfully seeded all data!"
+  end
+
+  def self.clear_prior_data
+    Campsite.destroy_all
+    Reservation.destroy_all
+  end
+
+  def self.seed_data
+    camp_1 = Campsite.create(name: "Cozy Cabin")
+    camp_2 = Campsite.create(name: "Comfy Cabin")
+    camp_3 = Campsite.create(name: "Rustic Cabin")
+    camp_4 = Campsite.create(name: "Rickety Cabin")
+    Campsite.create(name: "Cabin in the Woods")
+
+    Reservation.create(
+      campsite_id: camp_1.id,
+      start_date: Date.new(2018, 6, 1),
+      end_date: Date.new(2018, 6, 3),
+    )
+    Reservation.create(
+      campsite_id: camp_1.id,
+      start_date: Date.new(2018, 6, 8),
+      end_date: Date.new(2018, 6, 10),
+    )
+    Reservation.create(
+      campsite_id: camp_2.id,
+      start_date: Date.new(2018, 6, 1),
+      end_date: Date.new(2018, 6, 1),
+    )
+    Reservation.create(
+      campsite_id: camp_2.id,
+      start_date: Date.new(2018, 6, 2),
+      end_date: Date.new(2018, 6, 3),
+    )
+    Reservation.create(
+      campsite_id: camp_2.id,
+      start_date: Date.new(2018, 6, 7),
+      end_date: Date.new(2018, 6, 9),
+    )
+    Reservation.create(
+      campsite_id: camp_3.id,
+      start_date: Date.new(2018, 6, 1),
+      end_date: Date.new(2018, 6, 2),
+    )
+    Reservation.create(
+      campsite_id: camp_3.id,
+      start_date: Date.new(2018, 6, 8),
+      end_date: Date.new(2018, 6, 9),
+    )
+    Reservation.create(
+      campsite_id: camp_4.id,
+      start_date: Date.new(2018, 6, 7),
+      end_date: Date.new(2018, 6, 10),
+    )
+  end
+end
+
+Seed.start
