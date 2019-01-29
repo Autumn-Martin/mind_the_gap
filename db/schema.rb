4d0620100822cf8ceb44dd53d712745cebf8ae3e
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-29 13:57:07 -0700

    Create reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_203125) do
+ActiveRecord::Schema.define(version: 2019_01_29_204740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,14 @@ ActiveRecord::Schema.define(version: 2019_01_29_203125) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "campsite_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["campsite_id"], name: "index_reservations_on_campsite_id"
+  end
+
+  add_foreign_key "reservations", "campsites"
 end
