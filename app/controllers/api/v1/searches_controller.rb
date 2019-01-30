e5697573c262c43d68f61a825b1ac6a8cbbb36f3
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-29 21:07:32 -0700

    Add api::v1::searches index action

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::SearchesController < ApplicationController
+  def index
+    search_data = Api::V1::SearchesSerializer.new(params["start_date"], params["end_date"]).search_data
+    render json: search_data
+  end
 end
