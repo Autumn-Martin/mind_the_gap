8382a17e8c2441c8c39a2e6de3b5d01288f749c2
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-31 12:20:37 -0700

    Remove unused method

--- app/serializers/api/v1/searches_serializer.rb
+++ app/serializers/api/v1/searches_serializer.rb
@@ -11,24 +11,6 @@ class Api::V1::SearchesSerializer
     }
   end
 
-  def campsite_names
-    booked_campsite_ids = Reservation.booked_campsites #=> [1, 2, 3]
-    poss_campsite_ids = Campsite.where.not(id: booked_campsite_ids).pluck(:id) #=> [4, 5]
-    poss_campsites = Campsite.where.not(id: booked_campsite_ids)
-    prior_rezo_dates = poss_campsites.joins(:reservations).pluck(:start_date, :end_date)
-    
-    # return poss_campsites
-    ## poss_campsites = campsites that would not be double booked by query time range (poss available)
-    ## for each possible campsite
-    ##   if it has a reso that starts x (two) days after query range
-    ##   OR if it has a reso that starts x (two) days before query range
-           ## gap beware
-      ## else
-        ## available!
-
-  end
-
-
   private
 
   def serialized_campsites
