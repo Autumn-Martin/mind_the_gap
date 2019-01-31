985fca58f58aa59d4f0fe3b8b717bc7d82ae8a23
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-31 02:00:31 -0700

    Add route for api::v1::campsites endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
         get '/searches', to: 'searches#index'
+
+        get '/campsites/available', to: 'campsites#available'
       end
     end
 end
