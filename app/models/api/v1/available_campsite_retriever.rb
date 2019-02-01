5142c664d05d07e6dfec96844c3515d1fbeddb30
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-02-01 10:00:24 -0700

    Add Campsite class methods for retrieving potential & available campsites

--- app/models/api/v1/available_campsite_retriever.rb
+++ app/models/api/v1/available_campsite_retriever.rb
@@ -11,23 +11,10 @@ class Api::V1::AvailableCampsiteRetriever
     update_reservations
   end
 
-  def available_campsites
+  def updated_available_campsites
     update_data # if data is retrieved from an external API endpoint instead, this will update the internal database
 
-    booked_campsite_ids = Reservation.booked_campsites(@start_date, @end_date) #=> [1, 2, 3]
-    poss_campsites = Campsite.where.not(id: booked_campsite_ids)
-    available_spots = []
-
-    poss_campsites.each do |campsite|
-      campsite_rezo_dates = campsite.reservations.pluck(:start_date, :end_date)
-      if campsite_rezo_dates.include?(Date.strptime(@start_date, '%Y-%m-%d') - 2) && campsite_rezo_dates.exclude?(Date.strptime(@start_date, '%Y-%m-%d') - 1)
-      elsif campsite_rezo_dates.include?(Date.strptime(@end_date, '%Y-%m-%d') + 2) && campsite_rezo_dates.exclude?(Date.strptime(@end_date, '%Y-%m-%d') + 1)
-      else
-        available_spots << campsite
-      end
-    end
-
-    return available_spots.pluck(:name)
+    Campsite.available_campsite_names(@start_date, @end_date)
   end
 
   private
--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,4 +1,24 @@
 class Campsite < ApplicationRecord
   has_many :reservations
   validates_presence_of :name
+
+  def self.poss_campsites(query_start_date, query_end_date)
+    Campsite.where.not(id: Reservation.booked_campsite_ids(query_start_date, query_end_date))
+  end
+
+  def self.available_campsite_names(query_start_date, query_end_date)
+    available_spots = []
+
+    Campsite.poss_campsites(query_start_date, query_end_date).each do |campsite|
+      campsite_rezo_dates = campsite.reservations.pluck(:start_date, :end_date)
+
+      if campsite_rezo_dates.include?(Date.strptime(query_start_date, '%Y-%m-%d') - 2) && campsite_rezo_dates.exclude?(Date.strptime(query_start_date, '%Y-%m-%d') - 1)
+      elsif campsite_rezo_dates.include?(Date.strptime(query_end_date, '%Y-%m-%d') + 2) && campsite_rezo_dates.exclude?(Date.strptime(query_end_date, '%Y-%m-%d') + 1)
+      else
+        available_spots << campsite
+      end
+    end
+
+    return available_spots.pluck(:name)
+  end
 end
