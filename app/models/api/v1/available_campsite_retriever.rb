e27a43a32936efe30c98a50744b517bc69ebef38
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-31 19:22:43 -0700

    Remove unnec code

--- app/models/api/v1/available_campsite_retriever.rb
+++ app/models/api/v1/available_campsite_retriever.rb
@@ -6,9 +6,31 @@ class Api::V1::AvailableCampsiteRetriever
     @rezos = data[:reservations]
   end
 
-  def days
-    @start_date
+  def update_data
+    update_campsites
+    update_reservations
   end
+
+  def available_campsites
+    update_data # if data is retrieved from an external API endpoint instead, this will update the internal database
+
+    booked_campsite_ids = Reservation.booked_campsites #=> [1, 2, 3]
+    poss_campsites = Campsite.where.not(id: booked_campsite_ids)
+    available_spots = []
+
+    poss_campsites.each do |campsite|
+      campsite_rezo_dates = campsite.reservations.pluck(:start_date, :end_date)
+      if campsite_rezo_dates.include?(Date.strptime(@start_date, '%Y-%m-%d') - 2) && campsite_rezo_dates.exclude?(Date.strptime(@start_date, '%Y-%m-%d') - 1)
+      elsif campsite_rezo_dates.include?(Date.strptime(@end_date, '%Y-%m-%d') + 2) && campsite_rezo_dates.exclude?(Date.strptime(@end_date, '%Y-%m-%d') + 1)
+      else
+        available_spots << campsite
+      end
+    end
+
+    return available_spots.pluck(:name)
+  end
+
+  private
 
   def update_campsites
     if @campsites.count > Campsite.all.count
@@ -24,30 +46,5 @@ class Api::V1::AvailableCampsiteRetriever
         Reservation.find_or_create_by(campsite_id: rezo[:campsiteId], start_date: rezo[:startDate], end_date: rezo[:endDate])
       end
     end
-  end
-
-  def update_data
-    update_campsites
-    update_reservations
-  end
-
-  def available_campsites
-    update_data # if retrieve data from an external API endpoint instead, this will update the internal database
-    booked_campsite_ids = Reservation.booked_campsites #=> [1, 2, 3]
-    poss_campsite_ids = Campsite.where.not(id: booked_campsite_ids).pluck(:id) #=> [4, 5]
-    poss_campsites = Campsite.where.not(id: booked_campsite_ids)
-    prior_rezo_dates = poss_campsites.joins(:reservations).pluck(:start_date, :end_date)
-    available_spots = []
-    poss_campsites.each do |campsite|
-      campsite_rezo_dates = campsite.reservations.pluck(:start_date, :end_date)
-      if campsite_rezo_dates.include?(Date.strptime(@start_date, '%Y-%m-%d') - 2) && campsite_rezo_dates.exclude?(Date.strptime(@start_date, '%Y-%m-%d') - 1)
-        available_spots
-      elsif campsite_rezo_dates.include?(Date.strptime(@end_date, '%Y-%m-%d') + 2) && campsite_rezo_dates.exclude?(Date.strptime(@end_date, '%Y-%m-%d') + 1)
-        available_spots
-      else
-        available_spots << campsite
-      end
-    end
-    return available_spots.pluck(:name)
   end
 end
